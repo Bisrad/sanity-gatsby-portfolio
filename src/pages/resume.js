094b0e3a64c16c415f390ff2519d1e402e1ef7d0
Brad Thompson <bthompson6225@gmail.com>
2021-01-29 18:03:21 -0700

    Finished Static resume page

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -6,25 +6,30 @@ import { Col, Container, Row, Image, Button } from 'react-bootstrap';
 import img1 from '../images/ProfilePic.png';
 
 
-
 const ResumeStyles = styled.div`
     display: flex;
     justify-content: center;
     margin: 4rem;
-    padding: 2rem;
+ 
+        .img1 {
+            width: 250px;
+            padding: 0 65px;
+            margin-top: 3rem;
+            margin-bottom: 3rem;
+        }
         Col {
-            padding: 2rem;
+            padding: 0;
         }
         h2, h5 {
             text-align: center;
             margin: auto;
-
         }
 `;
 
 const MainButton = styled.div`
     display: flex;
     justify-content: flex-end;
+    border-radius: 3px!important;
 `;
 
 const ColoredLine = ({ color }) => (
@@ -32,11 +37,59 @@ const ColoredLine = ({ color }) => (
         style={{
             color: color,
             backgroundColor: color,
-            height: .4,
+            height: 1,
             width: 250
         }}
     />
 );
+
+class WorkExperience extends React.Component {
+
+    render() {
+        
+        return (
+            <div>
+                <Container>
+                    <Row style= {{ marginTop: 15 }}>
+                        <Col style= {{ padding: 0 }}>
+                            <h6>Job Title</h6> 
+                            <span style={{ marginRight: '2rem' }}> <em></em>  Company name</span>
+                            <span style={{ marginRight: '2rem' }}><em></em> Location</span>
+                        </Col>
+                        <Col style={{ textAlign: 'right', marginTop: '1rem' }}>
+                            <Button style={{ padding: 5, borderRadius: 50, border: 'none', backgroundColor: '#06BDD6', width: 125, justifyContent: 'flex-end' }}>Full Time</Button>
+                            <p style={{ marginRight: 8 }}> January 29, 2021 </p>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        )
+    }
+};
+class EducationBlock extends React.Component {
+
+    render() {
+        
+        return (
+            <div>
+                <Container>
+                    <Row style= {{ marginTop: 15 }}>
+                        <Col style= {{ padding: 0 }}>
+                            <h6>The Art Institute of Pittsburgh</h6> 
+                            <span style={{ marginRight: '2rem' }}> <em></em>  Associates of Visual Arts</span>
+                            <span style={{ marginRight: '2rem' }}><em></em> Phoenix, AZ</span>
+                        </Col>
+                        <Col style={{ textAlign: 'right', marginTop: '1.5rem' }}>
+                            <p style={{ marginTop: 25, marginRight: 8 }}> January 29, 2021 </p>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        )
+    }
+};
+
+
 
 
 class Resume extends React.Component {
@@ -46,11 +99,11 @@ class Resume extends React.Component {
             <ResumeStyles>
             <Container>
             <div>
-            <Link to="/">Back to Projects</Link>
+            <Link style={{ marginLeft: '4rem' }} to="/">⤎ Back to Projects</Link>
             </div>
             <Row>
             <Col>
-            <Image  src={img1} alt="profile pic" fluid/>
+            <Image className='img1' src={img1} alt="profile pic" fluid/>
             <h2>Brad Thompson</h2>
             <h5>Front End Developer</h5>
             <Row style={{ justifyContent: 'center', marginTop: 25 }}>
@@ -71,26 +124,37 @@ class Resume extends React.Component {
                 <br />
             </Row>
                 <ColoredLine color="#06BDD6" />
-            <Row style={{ justifyContent: 'center', marginTop: 25 }}>
+            <Row style={{ justifyContent: 'center', marginTop: 25, marginBottom: 45 }}>
                 <i style={{ marginRight: 15 }}>env   </i>
                 <span>Design & Development</span>
                 <br />
             </Row>
-                <ColoredLine color="#06BDD6" />
                 <Row style={{ justifyContent: 'center' }}>
                 <MainButton>
                         <Button 
                             variant="default"
-                            style={{width: 60, borderColor: '#06BDD6', backgroundColor: 'none', color: '#06BDD6'  }}
+                            style={{width: 230, borderColor: '#06BDD6', backgroundColor: 'none', color: '#06BDD6'  }}
                             >
-                            <Link style={{ margin: 0 }} to="/resume">Resume</Link>
+                            <Link style={{ margin: 0 }} to="/resume">Download Resume</Link>
                         </Button>    
                   </MainButton>
                 </Row>
             </Col>
             <Col>
-            <h1>Col2</h1>
-
+            <h1>Professional Summary</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla, porro, possimus quasi repudiandae accusamus dolorem distinctio pariatur quibusdam aspernatur est harum aut a. Temporibus quod recusandae quisquam adipisci dicta dignissimos ipsum, animi doloremque alias, mollitia repudiandae nulla itaque veritatis dolor quas sed laborum necessitatibus tempore!</p>
+            <Row>
+            <Container>
+            <h3>Work Experience</h3>
+            <WorkExperience />
+            <WorkExperience />
+            <WorkExperience />
+            </Container>
+            <Container>
+            <h3>Education</h3>
+            <EducationBlock />
+            </Container>
+            </Row>
             </Col>
             </Row>
             </Container>
