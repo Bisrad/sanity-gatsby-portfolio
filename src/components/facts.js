374e7a9b135248942142ed6783d90941d8c25c71
Brad Thompson <bthompson6225@gmail.com>
2021-01-31 22:05:52 -0700

    Add new facts Components

---
+++ src/components/facts.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import styled from 'styled-components';
+
+const LeftStyle = styled.div`
+    border-left: 1px;
+    border-left-style: solid;
+    padding: .5rem;
+        h5 {
+            padding-left: 15px;
+        }
+        p {
+            font-size: .8rem;
+            padding-left: 15px;
+        }
+`;
+
+class Facts extends React.Component {
+    render() {
+        return(
+                <Container style={{ backgroundColor: '#363636'}}>
+                    <Row>
+                        <LeftStyle>
+                            <Col xs={12} md={8} style={{ width: 200 }}>
+                            <h5>Fact 1</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quis nemo sit harum quam minus!</p>
+                            </Col>
+                            <Col xs={12} md={8} style={{ width: 200 }}>
+                            <h5>Fact 2</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quis nemo sit harum quam minus!</p>
+                            </Col>
+                            <Col xs={12} md={8} style={{ width: 250 }}>
+                            <h5>Fact 3</h5>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quis nemo sit harum quam minus!</p>
+                            </Col>
+                        </LeftStyle>
+                    </Row>
+                </Container>
+        );
+    }
+}
+export default Facts
